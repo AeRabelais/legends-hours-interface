8ef35dbff5cd62e5da483665cf63163f63cc3252
Ashia Lewis <pantagruelspendulum@protonmail.com>
2023-06-03 16:59:18 -0500

    updated input with more instruction

--- legends_hours/file_management/input.py
+++ legends_hours/file_management/input.py
@@ -1,4 +1,6 @@
-
 # Parse the uploaded excel sheet. Return the employee, employeeId, regularHours, start of week, and end of week.
 
 # Extract start of the week and end of the week from the name of the report file.
+
+# Return the parsed information for the excel file for the table.
+

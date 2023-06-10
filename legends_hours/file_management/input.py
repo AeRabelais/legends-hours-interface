2f8306a69c6a3bc135dbfe410d94cb79d80a92c2
Ashia Lewis <pantagruelspendulum@protonmail.com>
2023-06-10 12:50:12 -0500

    added a playground file for quick testing
    added numpy dependency
    
    updated column names
    
    corrected/finalized file parsing function
    
    added a test csv file

--- legends_hours/file_management/input.py
+++ legends_hours/file_management/input.py
@@ -1,5 +1,7 @@
+import uuid
+import numpy as np
 import pandas as pd 
-from settings import relevant_excel_cols
+from legends_hours.settings import relevant_excel_cols
 import re
 from datetime import datetime, timedelta
 
@@ -12,19 +14,31 @@ def parse_time_file(hours_file_path: str):
     time_df = ingest_time_file(hours_file_path)
 
     # Aggregate and group by the dataframe columns.
-    parsed_time_df = time_df.groupby(['Employee', 'EmployeeID']).agg({'Reg Hours': 'sum'}).reset_index()
+    parsed_time_df = time_df.groupby(['Employee']).agg({' Reg Hours': 'sum'}).reset_index()
 
-    # TODO: Make first name and last name columns that are a split of the employee name on the comma.
+    # Make first name and last name columns that are a split of the employee name on the comma.
+    parsed_time_df['first_name'] = parsed_time_df['Employee'].str.split(',').str[1].str.upper().str.strip()
+    parsed_time_df['last_name'] = parsed_time_df['Employee'].str.split(',').str[0].str.upper().str.strip()
 
-    # TODO: Add an id column that produces a unique UUID for each employee.
-    # TODO: Add flag column. Add values 0, to employees within the right time, 1 to employees ~36, 2 to employess ~40
+    # Add an id column that produces a unique UUID for each employee.
+    parsed_time_df['id'] = parsed_time_df.apply(lambda row: str(uuid.uuid4()), axis=1)
+
+    # Add flag column. Add values 0, to employees within the right time, 1 to employees ~36, 2 to employess ~40
+    conditions = [
+        parsed_time_df[' Reg Hours'] < 35,
+        (parsed_time_df[' Reg Hours'] >= 35) & (parsed_time_df[' Reg Hours'] < 40),
+        parsed_time_df[' Reg Hours'] >= 40
+    ]
+    choices = [0, 1, 2]
+
+    parsed_time_df['Flag'] = np.select(conditions, choices, default=0)
 
     # Add start date and end date columns.
     start_date, end_date = extract_week_from_title(hours_file_path)
 
     parsed_time_df["Start Date"] = start_date
     parsed_time_df["End Date"] = end_date
-    
+        
     return parsed_time_df 
 
 # Read in the time report file.
@@ -35,25 +49,22 @@ def ingest_time_file(hours_file_path: str):
     __check_columns_exist__(time_df)
     # Extract only the relevant columns.
     filtered_time_df = time_df[relevant_excel_cols]
-    filtered_time_df["Reg Hours"] = filtered_time_df["Reg Hours"].astype(int)
+    filtered_time_df[" Reg Hours"] = filtered_time_df[" Reg Hours"].astype(int)
 
     return filtered_time_df 
 
 # Extract the week start and end from the time report file.
 def extract_week_from_title(hours_file_path: str):
 
-    parsed_start_match = re.search("^\d{1,2}-\d{1,2}-\d{4}$", hours_file_path)
+    parsed_match = re.findall(r"\d{1,2}-\d{1,2}-\d{4}", hours_file_path)
 
-    if parsed_start_match is None:
+    if parsed_match is None:
         raise ValueError("No corresponding start date could be extracted for the input file.")
     else:
-        # Turn the parsed date into a datetime object.
-        start_date = parsed_start_match.group(0)
-        start_date_obj = datetime.strptime(start_date, '%m-%d-%y').date()
+        date_objects = [datetime.strptime(match, "%m-%d-%Y").date() for match in parsed_match]
+        print(date_objects)
 
-        # Add seven days to accounts for the end of a week.
-        end_date = start_date + timedelta(days=7)
-
+        start_date, end_date = date_objects[0], date_objects[1]
     return start_date, end_date
 
 def __check_file_format__(hours_file_path: str):
